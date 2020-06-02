8542a1288375cf85c4545298df5f5fdb636243c8
Christopher Pence <cpence@ucsd.edu>
2020-06-01 19:55:31 -0700

    Cleanup and extra validation parameter + favicon

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ const schema = yup.object({
   maxCapacity: yup.number().min(1, 'Must have at least one spot of capacity.').required('A maximum capacity is required.'),
   reservedSeating: yup.number().min(0, 'Cannot have a negative number of seats.').max(yup.ref('maxCapacity'), 'Reserved seating cannot exceed maximum capacity.').required('Reserved seating is required.'),
   separationRadius: yup.number().required('A separation radius is required.'),
-  seatWidth: yup.number().required('A seat width is required.'),
+  seatWidth: yup.number().min(1, 'Seats must have a positive non-zero width.').required('A seat width is required.'),
   pewFile: yup.mixed().required('A pew file is required.').test('fileFormat', 'Must be a CSV file.', isCsvType),
   familyFile: yup.mixed().required('A household file is required.').test('fileFormat', 'Must be a CSV file.', isCsvType),
 });
