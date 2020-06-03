91a188d36e92e59a468e9165e11149c0163d35b8
Christopher Pence <cpence@ucsd.edu>
2020-06-02 22:16:32 -0700

    Added controlled file input

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import Form from 'react-bootstrap/Form';
+
+const ControlledFileInput = ({ value, ...rest }) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (!value) {
+      ref.current.value = null;
+    }
+  }, [value]);
+
+  return <input {...rest} ref={ref} type="file" />;
+};
+
 
 const FileInput = ({ label, placeholder, explanation, field, form, meta, ...props }) => {
   return (
@@ -9,13 +22,14 @@ const FileInput = ({ label, placeholder, explanation, field, form, meta, ...prop
       <Form.File
         label={field.value?.name || placeholder}
         custom
+        value={field.value}
+        inputAs={ControlledFileInput}
         feedback={meta.error}
         name={field.name}
         onBlur={field.onBlur}
         onChange={(event) => {
-          const file = event.currentTarget.files[0];
-          const fileValue = file ? file : null;
-          form.setFieldValue(field.name, fileValue);
+          const file = event.currentTarget.files[0] || null;
+          form.setFieldValue(field.name, file);
         }}
         {...props}
       />
